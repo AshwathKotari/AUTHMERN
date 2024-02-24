8b66f81bacbc5f8ad3dfec19ae35e787eeae04ec
AshwathKotari <ashwathkotari1996@gmail.com>
2024-02-24 17:20:40 +0530

    MERN AUth APp

---
+++ client/src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+import { BrowserRouter , Routes , Route } from 'react-router-dom'
+import { Home } from './pages/Home'
+import { About } from './pages/About'
+import { Profile } from './pages/Profile'
+import { SignIn } from './pages/SignIn'
+import { SignUp } from './pages/SignUp'
+
+
+function App() {
+  
+
+  return (
+    <>
+     <BrowserRouter>
+     <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/About' element={<About/>} />
+        <Route path='/Profile' element={<Profile/>} />
+        <Route path='/SignIn' element={<SignIn/>} />
+        <Route path='/SignUp' element={<SignUp/>} />
+     </Routes>
+     </BrowserRouter>
+    </>
+  )
+}
+
+export default App
